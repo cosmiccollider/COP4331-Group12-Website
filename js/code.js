0dc22376db78e17bf278a36df6439ded14103fb5
Justin Gifford <49419781+JGiffordX@users.noreply.github.com>
2023-06-01 10:58:04 -0400

    Classes, focusing, and error checking
    - Added Add Contact page
    - Added automatic formatting for phone numbers and birthdays on Add Contact page
    - Added errors for incorrect/invalid usernames and passwords on login and register pages (currently only checks for placeholders)
    - Added mainContent class to group divs together and simplify CSS
    - Added inputText class to group input fields together and simplify CSS
    - Added the ability to change focus of input fields by pressing the Enter key
    - Added the ability to login/register by pressing the Enter key on the last input field of each respective page
    - Added doctype, lang, and meta tags to html files
    - Added md5.js file
    - Redesigned color palette
    - Increased differentiation between primary buttons and secondary buttons
    - Changed mainButton ids to be more descriptive, added mainButton class to retain consistent CSS
    - Changed ids of Login and Register page input fields to better reflect their unique purpose
    - Changed design of focused input fields to match site design
    - Changed Add Contact button on Contacts page to be an image instead of text
    - Changed buttons to be more reactive upon click for greater feedback to user
    - Changed navigation buttons between login and register pages to hyperlinks, prompting the user on whether they have an account or not
    - Changed buttons and hyperlinks outline to appear when focused by keyboard input (such as tabbing)
    - Fixed a slight centering issue on divs by changing sizing to be based on margins rather than width
    - Fixed an issue where password visibility icons had a slight delay in loading when going from home page to login/register pages
    - Condensed toggleVisibility() function for greater simplicity and scalability
    - Transferred style attributes in HTML files to CSS file for consistency
    - Seperated HTML into blocks for better readability

--- js/code.js
+++ js/code.js
@@ -1,4 +1,4 @@
-const urlBase = 'http://127.0.0.1:5500/UCF/COP%204331/Small%20Project';
+const urlBase = 'http://cop4331project.xyz/LAMPAPI';
 const extension = 'php';
 
 let userId = 0;
@@ -7,7 +7,12 @@ let lastName = "";
 
 // ----- Navigation Functions -----
 
-function goContacts() // DEBUG ONLY
+function goAdd()
+{
+	window.location.href = "add.html";
+}
+
+function goContacts()
 {
 	window.location.href = "contacts.html";
 }
@@ -27,57 +32,106 @@ function goRegister()
 	window.location.href = "register.html";
 }
 
-// ----- Account Functions -----
+// ----- Login Functions -----
+
+// Clears any displayed error of a matching field id
+function clearError(field)
+{
+	let error = document.getElementById(field.id + "Error");
+	error.innerHTML = "";
+	error.style.display = "none";
+}
+
+// Checks that both password fields match when registering
+function confirmPassword()
+{
+	let password = document.getElementById("registerPassword").value;
+	let confirm = document.getElementById("confirmPassword").value;
+	let confirmError = document.getElementById("confirmPasswordError");
+
+	if (confirm != password)
+	{
+		confirmError.innerHTML = "Passwords don't match";
+		confirmError.style.display = "inline-block";
+	}
+}
 
 function doLogin()
 {
 	userId = 0;
 	firstName = "";
 	lastName = "";
-	
+
 	let username = document.getElementById("loginUsername").value;
 	let password = document.getElementById("loginPassword").value;
-//	var hash = md5( password );
-	
-	document.getElementById("loginResult").innerHTML = "";
+	let usernameError = document.getElementById("loginUsernameError");
+	let passwordError = document.getElementById("loginPasswordError");
+	// var hash = md5(password);
 
-	let tmp = {username:username,password:password};
-//	var tmp = {username:username,password:hash};
-	let jsonPayload = JSON.stringify( tmp );
-	
-	let url = urlBase + '/Login.' + extension;
+	// Placeholders
+	let user = "user";
+	let pass = "pass";
 
+	if (username == "")
+	{
+		usernameError.innerHTML = "Invalid username";
+		usernameError.style.display = "inline-block";
+		return;
+	}
+	
+	if (username != user)
+	{
+		usernameError.innerHTML = "User not found"
+		usernameError.style.display = "inline-block";
+		return;
+	}
+
+	if (password != pass)
+	{
+		passwordError.innerHTML = "Incorrect password";
+		passwordError.style.display = "inline-block";
+		return;
+	}
+
+	goContacts(); // DEBUG
+
+	// Do Login
+
+	let tmp = {username:username, password:password};
+	// var tmp = {username:username, password:hash};
+	let jsonPayload = JSON.stringify(tmp);
+	let url = urlBase + "/Login." + extension;
 	let xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
 	try
 	{
 		xhr.onreadystatechange = function()
 		{
 			if (this.readyState == 4 && this.status == 200)
 			{
-				let jsonObject = JSON.parse( xhr.responseText );
+				let jsonObject = JSON.parse(xhr.responseText);
 				userId = jsonObject.id;
-		
-				if( userId < 1 )
+
+				if (userId < 1)
 				{
-					document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+					console.log("Incorrect login");
 					return;
 				}
-		
+
 				firstName = jsonObject.firstName;
 				lastName = jsonObject.lastName;
-
 				saveCookie();
-	
 				window.location.href = "contacts.html";
 			}
 		};
+
 		xhr.send(jsonPayload);
 	}
-	catch(err)
+	catch (err)
 	{
-		document.getElementById("loginResult").innerHTML = err.message;
+		console.log(err.message);
 	}
 }
 
@@ -96,43 +150,202 @@ function doRegister()
 	firstName = "";
 	lastName = "";
 	
-	let username = document.getElementById("loginUsername").value;
-	let password = document.getElementById("loginPassword").value;
+	let username = document.getElementById("registerUsername").value;
+	let password = document.getElementById("registerPassword").value;
 	let confirm = document.getElementById("confirmPassword").value;
+	let usernameError = document.getElementById("registerUsernameError");
+	let passwordError = document.getElementById("registerPasswordError");
+	let confirmError = document.getElementById("confirmPasswordError");
+	
+	// Validate that all fields have correct input data
+
+	// Check in sequential order that no fields were left empty
+	if (username == "")
+	{
+		usernameError.innerHTML = "Invalid username";
+		usernameError.style.display = "inline-block";
+	}
+	else if (password == "")
+	{
+		passwordError.innerHTML = "Invalid password";
+		passwordError.style.display = "inline-block";
+	}
+	else if (confirm == "")
+	{
+		confirmError.innerHTML = "Passwords don't match";
+		confirmError.style.display = "inline-block";
+	}
+
+	// Check if there are any active errors still being displayed
+	if (usernameError.style.display == "inline-block"
+	|| passwordError.style.display == "inline-block"
+	|| confirmError.style.display == "inline-block")
+	{
+		return;
+	}
+
+	goContacts(); // DEBUG
+
+	// Do Register
+}
+
+function registerPassword()
+{
+	let password = document.getElementById("registerPassword").value;
+	let passwordError = document.getElementById("registerPasswordError");
+	let valid = new RegExp("(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})");
+
+	// Checks that a password satisfies complexity requirement
+	// If a password is currently failing, notifies the user what's wrong with it
+	if (!valid.test(password))
+	{
+		passwordError.innerHTML = "Password must contain:";
+		passwordError.style.display = "inline-block";
+
+		let upper = new RegExp("(?=.*[A-Z])");
+		let lower = new RegExp("(?=.*[a-z])");
+		let number = new RegExp("(?=.*[0-9])");
+		let symbol = new RegExp("(?=.*[^A-Za-z0-9])");
+		let length = new RegExp("(?=.{8,})");
+		
+		if (!upper.test(password))
+		{
+			passwordError.innerHTML += "\r\nUppercase letter";
+		}
+		if (!lower.test(password))
+		{
+			passwordError.innerHTML += "\r\nLowercase letter";
+		}
+		if (!number.test(password))
+		{
+			passwordError.innerHTML += "\r\nNumber";
+		}
+		if (!symbol.test(password))
+		{
+			passwordError.innerHTML += "\r\nSymbol";
+		}
+		if (!length.test(password))
+		{
+			passwordError.innerHTML += "\r\nAt least 8 characters";
+		}
+	}
+}
+
+function registerUsername()
+{
+	let username = document.getElementById("registerUsername").value;
+	let usernameError = document.getElementById("registerUsernameError");
+
+	// Example feature
+	if (username == "user")
+	{
+		usernameError.innerHTML = username + " already exists";
+		usernameError.style.display = "inline-block";
+	}
 }
 
 // ----- Event Functions -----
+
+// Formats birthday input field as XX/XX or XX/XX/XXXX depending on number of digits
+function birthday()
+{
+	let birthdayField = document.getElementById("birthday");
+	let birthdayValue = birthdayField.value.replace(/\D/g, "");
+
+	if (birthdayValue.length >= 5)
+	{
+		birthdayField.value = birthdayValue.slice(0, 2) + "/" + birthdayValue.slice(2, 4) + "/" + birthdayValue.slice(4, 8);
+	}
+	else if (birthdayValue.length >= 3 && birthdayValue.length <= 4)
+	{
+		birthdayField.value = birthdayValue.slice(0, 2) + "/" + birthdayValue.slice(2);
+	}
+	else
+	{
+		birthdayField.value = birthdayValue.slice(0, 2);
+	}
+}
+
+// Allows for logging in and registering by pressing "Enter" on the final input field
+// On other input fields, allows for moving focus to the next input field
+function keyInput(field)
+{
+	document.getElementById(field.id).addEventListener("keyup", function (event)
+	{
+		if (event.key === "Enter")
+		{
+			switch (field.id)
+			{
+				case "loginPassword":
+					doLogin();
+					break;
+				case "confirmPassword":
+					doRegister();
+					break;
+				default:
+					let next = field.nextElementSibling;
+					while (next.className != "inputText")
+					{
+						next = next.nextElementSibling;
+					}
+					next.focus();
+			}
+		}
+	}, {once : true});
+}
+
+// Formats phone number input field as XXX-XXXX or (XXX) XXX-XXXX depending on number of digits
+function phoneNumber()
+{
+	let phoneField = document.getElementById("phoneNumber");
+	let phoneValue = phoneField.value.replace(/\D/g, "");
+
+	if (phoneValue.length >= 8)
+	{
+		phoneField.value = "(" + phoneValue.slice(0, 3) + ") " + phoneValue.slice(3, 6) + "-" + phoneValue.slice(6, 10);
+	}
+	else if (phoneValue.length >= 4 && phoneValue.length <= 7)
+	{
+		phoneField.value = phoneValue.slice(0, 3) + "-" + phoneValue.slice(3);
+	}
+	else
+	{
+		phoneField.value = phoneValue.slice(0, 3);
+	}
+}
 
 function readCookie()
 {
 	userId = -1;
 	let data = document.cookie;
 	let splits = data.split(",");
+
 	for(var i = 0; i < splits.length; i++)
 	{
 		let thisOne = splits[i].trim();
 		let tokens = thisOne.split("=");
-		if( tokens[0] == "firstName" )
+
+		if(tokens[0] == "firstName")
 		{
 			firstName = tokens[1];
 		}
-		else if( tokens[0] == "lastName" )
+		else if(tokens[0] == "lastName")
 		{
 			lastName = tokens[1];
 		}
-		else if( tokens[0] == "userId" )
+		else if(tokens[0] == "userId")
 		{
-			userId = parseInt( tokens[1].trim() );
+			userId = parseInt(tokens[1].trim());
 		}
 	}
-	
-	if( userId < 0 )
+
+	if(userId < 0)
 	{
 		window.location.href = "index.html";
 	}
 	else
 	{
-		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+		console.log("Logged in as: " + firstName + " " + lastName);
 	}
 }
 
@@ -140,7 +353,7 @@ function saveCookie()
 {
 	let minutes = 20;
 	let date = new Date();
-	date.setTime(date.getTime()+(minutes*60*1000));
+	date.setTime(date.getTime() + (minutes * 60 * 1000));
 	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
 }
 
@@ -148,14 +361,30 @@ function saveCookie()
 
 function addContact()
 {
+	let first = document.getElementById("firstName").value;
+	let last = document.getElementById("lastName").value;
+	let email = document.getElementById("email").value;
+	let phone = document.getElementById("phoneNumber").value;
+	let birthday = document.getElementById("birthday").value;
+	
+	// Validate first name isn't empty (last name can be empty)
+
+	// Validate email is format user@site.ext (can be empty)
+
+	// Validate phone number XXX-XXXX or (XXX) XXX-XXXX (can be empty)
+
+	// Validate birthday is XX/XX/XXXX (can be empty)
+	
+	goContacts(); // DEBUG
+
+	// Add Contact
+	
 	let newContact = document.getElementById("contactText").value;
 	document.getElementById("addContactResult").innerHTML = "";
-
+	
 	let tmp = {contact:newContact,userId,userId};
 	let jsonPayload = JSON.stringify( tmp );
-
 	let url = urlBase + '/AddContact.' + extension;
-	
 	let xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
@@ -191,13 +420,11 @@ function searchContacts()
 	let srch = document.getElementById("searchText").value;
 	document.getElementById("searchContactsResult").innerHTML = "";
 	
-	let colorList = "";
-
-	let tmp = {search:srch,userId:userId};
-	let jsonPayload = JSON.stringify( tmp );
-
-	let url = urlBase + '/SearchContacts.' + extension;
+	let contactList = "";
 	
+	let tmp = {search:srch,userId:userId};
+	let jsonPayload = JSON.stringify(tmp);
+	let url = urlBase + '/SearchContacts.' + extension;
 	let xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
@@ -212,14 +439,14 @@ function searchContacts()
 				
 				for( let i=0; i<jsonObject.results.length; i++ )
 				{
-					colorList += jsonObject.results[i];
+					contactList += jsonObject.results[i];
 					if( i < jsonObject.results.length - 1 )
 					{
-						colorList += "<br />\r\n";
+						contactList += "<br />\r\n";
 					}
 				}
 				
-				document.getElementsByTagName("p")[0].innerHTML = colorList;
+				document.getElementsByTagName("p")[0].innerHTML = contactList;
 			}
 		};
 		xhr.send(jsonPayload);
@@ -230,28 +457,22 @@ function searchContacts()
 	}
 }
 
-function toggleVisibility(confirmPassword)
+// Shows or hides the text of a password input field
+function toggleVisibility(field)
 {
-	if (confirmPassword)
-	{
-		var icon = document.getElementById("confirmVisibility");
-		var text = document.getElementById("confirmPassword");
-	}
-	else
-	{
-		var icon = document.getElementById("passwordVisibility");
-		var text = document.getElementById("loginPassword");
-	}
+	let icon = document.getElementById(field.id);
+	let str = field.id.slice(0, field.id.length - 10);
+	let text = document.getElementById(str + "Password");
 
 	if (text.type == "password")
 	{
-		icon.textContent = "visibility_off";
+		icon.innerHTML = "visibility_off";
 		icon.title = "Hide Password";
 		text.type = "text";
 	}
 	else
 	{
-		icon.textContent = "visibility";
+		icon.innerHTML = "visibility";
 		icon.title = "Show Password";
 		text.type = "password";
 	}
