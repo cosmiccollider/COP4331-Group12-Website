e59710bd43c4d3351c7afadccf818434ac570015
KimLe10 <135267066+KimLe10@users.noreply.github.com>
2023-06-05 03:27:23 -0400

    order fix

--- LAMPAPI/UpdateContact.php
+++ LAMPAPI/UpdateContact.php
@@ -11,7 +11,7 @@
 
 	$conf = json_decode(file_get_contents('conf.json'), true);
     $conn = new mysqli($conf['hostname'], $conf['username'], $conf['password'], $conf['database']);
-	//$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	
 	if ($conn->connect_error)
 	{
 		returnWithError( $conn->connect_error );
@@ -19,7 +19,7 @@
 	else
 	{
 		$stmt = $conn->prepare("UPDATE Contacts SET FirstName = ?, LastName = ?, Phone = ?, Email = ?, Birthday = ? WHERE ID = ?");
-		$stmt->bind_param("ssssss", $newFirstName, $newLastName, $newPhoneNumber, $newEmail, $ID, $newBirthDay);
+		$stmt->bind_param("ssssss", $newFirstName, $newLastName, $newPhoneNumber, $newEmail, $newBirthDay, $ID);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
